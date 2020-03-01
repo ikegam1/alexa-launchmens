de9c7794362463a156c604adc6770a66a6c8ffcc
User1 <user1@yuza1noMacBook-ea.local>
2020-03-01 18:36:31 +0900

    release

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,7 +6,6 @@ import re
 import os
 import sys
 import utils
-import langs
 
 app = Chalice(app_name='alexa-launchmens')
 logger = logging.getLogger()
@@ -20,7 +19,7 @@ else:
 drumrole_mp3 = "soundbank://soundlibrary/musical/amzn_sfx_drum_and_cymbal_01"
 
 @app.lambda_function()
-def default(event, context):
+def lambda_handler(event, context):
     request = event['request']
     request_type = request['type']
     session = {}
@@ -29,11 +28,10 @@ def default(event, context):
         return questionIntent()
     elif request_type == 'IntentRequest' and 'intent' in request:
         if 'dialogState' in request and request['dialogState'] != 'COMPLETED': 
-           logger.info('run DialogDelegate()')
-           #return utils.DialogDelegate().build()
-           return onDialogState(request, request['intent'], request['dialogState'])
+            logger.info('run DialogDelegate()')
+            return onDialogState(request, request['intent'], request['dialogState'])
         else:
-           return in_intent(request, session)
+            return in_intent(request, session)
 
 def in_intent(request, session):
     intent = request['intent']
@@ -89,22 +87,22 @@ def answerIntent(request, intent, slots):
         ]
         _ans_second = [
             {},
-            {"a","餃子とライス", "i":"餃子ってなんでこんなにご飯に合うんですかえね。ニンニクの匂いにはご注意を。"},
+            {"a":"餃子とライス", "i":"餃子ってなんでこんなにご飯に合うんですかえね。ニンニクの匂いにはご注意を。"},
             {"a":"トンカツ", "i":"豚肉といえばトンカツ、トンカツといえば豚肉ですよね。ライスもお替わりしちゃいましょう。"},
             {"a":"ちゃんこ鍋", "i":"昼からちゃんこを食べてもいいじゃない。どすこい。"}
         ]
         _ans_third = [
             {},
-            {"a","プロテイン", "i":"血となり肉となれタンパク質。炭水化物とかいりません。"},
-            {"a","拾ったおにぎり", "i":"<break time=\"0.5s\" />。。大丈夫でしょうか。。おなか壊さないでくださいね。"},
+            {"a":"プロテイン", "i":"血となり肉となれタンパク質。炭水化物とかいりません。"},
+            {"a":"拾ったおにぎり", "i":"<break time=\"0.5s\" />。。大丈夫でしょうか。。おなか壊さないでくださいね。"},
             {"a":"スニッカーズ", "i":"疲れた脳には糖分が最高ですよね。でも適度に休憩をとってくださいね。。"}
         ]
         _matches = []
         _first = slots['firstSlot']['value']
         _second = slots['secondSlot']['value']
         _third = slots['thirdSlot']['value']
-        _matches = [_ans_first[int(_first)], _ans_second[int(_second)], _ans_third[int(_third)]
-        answer = _matches[random.choice(range(1,3)]
+        _matches = [_ans_first[int(_first)], _ans_second[int(_second)], _ans_third[int(_third)]]
+        answer = _matches[random.choice(range(1,3))]
         
     except Exception as e:
         answer = {"a":"チキン","i":""}
@@ -114,9 +112,9 @@ def answerIntent(request, intent, slots):
     text = '今日のあなたのランチは<break time="0.5s"/>'
     text += '<audio src="%s" />' % drumrole_mp3
 
-    if ptn == 1:
-        text += u'<prosody rate="105%"><prosody volume="+1dB">' + answer['a'] + 'がオススメです！</prosody></prosody>'
-        text += '<break time="0.5s" />' + answer['i'] + '。'
-        text += u'<break time="1.0s" />遊んでくれてありがとう。ではまた！'
+
+    text += u'<prosody rate="105%"><prosody volume="+1dB">' + answer['a'] + 'がオススメです！</prosody></prosody>'
+    text += '<break time="0.5s" />' + answer['i'] + '。'
+    text += u'<break time="1.0s" />遊んでくれてありがとう。ではまた！'
 
     return utils.OneSpeech(text).build()
